184e222f3fe35a9c4e01ebfc6767ce89f98a316e
MaVasil <mdvasil07@gmail.com>
2026-03-20 15:07:37 +0530

    Time: 672 ms (25.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,14 @@
+SELECT employee_id, department_id
+FROM employee
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+)
+
+UNION
+
+SELECT employee_id, department_id
+FROM employee
+WHERE primary_flag = 'Y';
